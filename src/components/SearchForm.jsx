692b36ec836e936adaa6745bdde28443276d2622
Nikita Konkin <niki-konkin@yandex.ru>
2022-03-01 01:54:19 +0300

    search - done

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,20 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+
+
 
 
 
 export default function SearchForm() {
+
+  const [tuggleSwitchState, setTuggleSwitchState] = useState(false);
+
+  function handleTuggleSwitchState() {
+    return setTuggleSwitchState(!tuggleSwitchState)
+  }
+
+
   return (
     <form className="form-search">
       <div className="form-search__container">
-        <input type="text" name="search-box" className="form-search__input"/>
-        <input type="submit" value="Submit" className="form-search__btn-submit"/>
+        <input type="text" placeholder="Фильм" name="search-box" className="form-search__input"/>
+        <input type="submit" value=" " src="../images/find_btn.svg" className="form-search__btn-submit"/>
       </div>
         <label className="form-search__lable">
           Короткометражки
         </label>
-        <button onclick="" className="form-search__btn-switch">
-          btn
-        </button>
+        <lable className="form-search__btn-switch-container">
+          <input type="checkbox" className="form-search__btn-switch" checked={tuggleSwitchState} />
+          <span className="form-search__btn-switch-slider" onClick={handleTuggleSwitchState}></span>
+        </lable>
+
     </form>
   );
 }
