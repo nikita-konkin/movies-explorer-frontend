834486319aaec127d1537ff3f0c9512d553a3cf8
Nikita Konkin <niki-konkin@yandex.ru>
2022-03-06 01:59:27 +0300

    all - done

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import myFoto from '../images/my_foto.png';
 
 export default function AboutMe() {
-  return (
-    <div>
-      <h2 className="about__header">О проекте</h2>
 
+
+
+  return (
+    <div className="aboutme">
+      <h2 className="aboutme__header">Студент</h2>
+      <div className="aboutme__info-container">
+          <h2 className="aboutme__info-name">Никита</h2>
+          <h3 className="aboutme__info-job">Фронтенд-разработчик, 30 лет</h3>
+          <p className="aboutme__info-story">Я родился и живу в Саратове, закончил факультет экономики СГУ.
+            У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
+            бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
+            Контур». После того, как прошёл курс по веб-разработке, начал
+            заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <img src={myFoto} className="aboutme__foto" alt="Никита"/>
+          <div className="aboutme__link">
+            <a className="aboutme__link-fb">Facebook</a>
+            <a className="aboutme__link-gh">Github</a>
+          </div>
+
+      </div>
     </div>
   );
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import arrowImg from '../images/src_arrow.svg'
 export default function Portfolio() {
   return (
-    <>
-      Portfolio
-    </>
+    <div className="portfolio">
+      <h3 className="portfolio__header">Портфолио</h3>
+      <div className="portfolio__container">
+        <div className="portfolio__link-container">
+          <a href="url" className="portfolio__link">Статичный сайт</a>
+          <img src={arrowImg}  className="portfolio__link-arrow"></img>
+        </div>
+        <div className="portfolio__link-container">
+          <a href="url"  className="portfolio__link">Адаптивный сайт</a>
+          <img src={arrowImg} className="portfolio__link-arrow"></img>
+        </div>
+        <div className="portfolio__link-container">
+          <a href="url"  className="portfolio__link">Одностраничное приложение</a>
+          <img src={arrowImg} className="portfolio__link-arrow"></img>
+        </div>
+      </div>
+    </div>
   );
 }
